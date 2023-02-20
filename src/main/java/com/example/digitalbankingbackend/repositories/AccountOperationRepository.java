bc2a02bf6cb7d477c3eb6b0aef53e04162731fbc
hamid <74140209+hamid423@users.noreply.github.com>
2023-02-20 20:19:01 +0100

    repos

---
+++ src/main/java/com/example/digitalbankingbackend/repositories/AccountOperationRepository.java
@@ -0,0 +1,7 @@
+package com.example.digitalbankingbackend.repositories;
+
+import com.example.digitalbankingbackend.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/com/example/digitalbankingbackend/repositories/CustomerRepository.java
@@ -0,0 +1,2 @@
+package com.example.digitalbankingbackend.repositories;public interface CustomerRepository {
+}
