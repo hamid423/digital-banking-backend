66bc38b3e222ade2e838fccdc7b51e9800fc2108
hamid <74140209+hamid423@users.noreply.github.com>
2023-02-20 11:07:11 +0100

    entities

---
+++ src/main/java/com/example/digitalbankingbackend/entities/AccountOperation.java
@@ -0,0 +1,22 @@
+package com.example.digitalbankingbackend.entities;
+
+import com.example.digitalbankingbackend.enums.OperationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class AccountOperation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    @ManyToOne
+    private BankAccount bankAccount;
+
+}
---
+++ src/main/java/com/example/digitalbankingbackend/enums/OperationType.java
@@ -0,0 +1,4 @@
+package com.example.digitalbankingbackend.enums;
+
+public enum OperationType {
+}
